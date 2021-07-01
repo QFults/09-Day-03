6d19731de3f709ae889ee19b1da4e66dd7b1835b
Quinton Fults <quinton.fults.work@gmail.com>
2021-07-01 14:21:55 -0700

    code

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,34 +1,40 @@
 const router = require('express').Router()
 const { Dog, Owner } = require('../models')
+const passport = require('passport')
 
 Dog.obliterate = Dog.findByIdAndDelete
 
 // GET all dogs
-router.get('/dogs', (req, res) => Dog.find()
+router.get('/dogs', passport.authenticate('jwt'), (req, res) => Dog.find()
   .populate('owner')
   .then(dogs => res.json(dogs))
   .catch(err => console.log(err)))
 
 // GET one dog
-router.get('/dogs/:id', (req, res) => Dog.findById(req.params.id)
+router.get('/dogs/:id', passport.authenticate('jwt'), (req, res) => Dog.findById(req.params.id)
   .populate('owner')
   .then(dog => res.json(dog))
   .catch(err => console.log(err)))
 
 // POST one dog
-router.post('/dogs', (req, res) => Dog.create(req.body)
+router.post('/dogs', passport.authenticate('jwt'), (req, res) => Dog.create({
+  name: req.body.name,
+  breed: req.body.breed,
+  age: req.body.age,
+  owner: req.user._id
+})
   .then(dog => Owner.findByIdAndUpdate(dog.owner, { $push: { dogs: dog._id } })
     .then(() => res.json(dog))
     .catch(err => console.log(err)))
   .catch(err => console.log(err)))
 
 // PUT one dog
-router.put('/dogs/:id', (req, res) => Dog.findByIdAndUpdate(req.params.id, { $set: req.body })
+router.put('/dogs/:id', passport.authenticate('jwt'), (req, res) => Dog.findByIdAndUpdate(req.params.id, { $set: req.body })
   .then(() => res.sendStatus(200))
   .catch(err => console.log(err)))
 
 // DELETE one dog
-router.delete('/dogs/:id', (req, res) => Dog.obliterate(req.params.id)
+router.delete('/dogs/:id', passport.authenticate('jwt'), (req, res) => Dog.obliterate(req.params.id)
   .then(() => res.sendStatus(200))
   .catch(err => console.log(err)))
 
--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,32 +1,46 @@
 const router = require('express').Router()
 const { Owner } = require('../models')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
 Owner.obliterate = Owner.findByIdAndDelete
 
+router.post('/owners/register', (req, res) => {
+  const { name, email, username } = req.body
+  Owner.register(new Owner({ name, email, username }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.post('/owners/login', (req, res) => {
+  Owner.authenticate()(req.body.username, req.body.password, (err, owner) => {
+    if (err) { console.log(err) }
+    res.json(owner ? jwt.sign({ id: owner._id }, process.env.SECRET) : null)
+  })
+})
+
 // GET all owner
-router.get('/owners', (req, res) => Owner.find()
+router.get('/owners', passport.authenticate('jwt'), (req, res) => Owner.find()
   .populate('dogs')
   .then(owner => res.json(owner))
   .catch(err => console.log(err)))
 
 // GET one owner
-router.get('/owners/:id', (req, res) => Owner.findById(req.params.id)
-  .populate('dogs')
-  .then(owner => res.json(owner))
-  .catch(err => console.log(err)))
+router.get('/owner', passport.authenticate('jwt'), (req, res) => res.json(req.user))
 
 // POST one owner
-router.post('/owners', (req, res) => Owner.create(req.body)
-  .then(owner => res.json(owner))
-  .catch(err => console.log(err)))
+// router.post('/owners', passport.authenticate('jwt'), (req, res) => Owner.create(req.body)
+//   .then(owner => res.json(owner))
+//   .catch(err => console.log(err)))
 
 // PUT one owner
-router.put('/owners/:id', (req, res) => Owner.findByIdAndUpdate(req.params.id, { $set: req.body })
+router.put('/owners', passport.authenticate('jwt'), (req, res) => Owner.findByIdAndUpdate(req.user._id, { $set: req.body })
   .then(() => res.sendStatus(200))
   .catch(err => console.log(err)))
 
 // DELETE one owner
-router.delete('/owners/:id', (req, res) => Owner.obliterate(req.params.id)
+router.delete('/owners', passport.authenticate('jwt'), (req, res) => Owner.obliterate(req.user._id)
   .then(() => res.sendStatus(200))
   .catch(err => console.log(err)))
 
